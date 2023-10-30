7b0dc25992f557a4f133130a297c2c46233f0613
Hiba Khalid <79014554+HibaKhalid2504@users.noreply.github.com>
2023-10-30 16:46:37 +0500

    Second commit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,22 @@ const router=express.Router();
 
 import {createUser,getAllUsers,getSingleUser,updateSingleUser,deleteSingleUser} from "../controller/user.controller.js"
 
+// We can have multiple middlewares.
+const middleware=(req,res,next)=>{
+    console.log("Middleware calling......")
+    next()
+}
+
+const middleware2=(req,res,next)=>{
+    console.log("Middleware2 calling......")
+    next()
+}
+
 router.post("/user", createUser)
-router.get("/user",getAllUsers)
+router.get("/user",middleware,getAllUsers)
+//router.get("/user",[middleware,middleware2],getAllUsers)
+
+
 router.get("/user/:id",getSingleUser)
 //Method used to find by name
 //router.get("/user/:name",getSingleUser)
